d8c88c5dfe0b20b37f7237fe5b15353f1044638c
Arunshah1919 <arunshah1919@gmail.com>
2022-08-04 17:10:41 +0530

    dsgadf

--- JS/forpush.js
+++ JS/forpush.js
@@ -7,10 +7,19 @@ for (let a = 0; a<=10; a++){
 console.log(myarray);
 */
 
-let myarray = [];
+/*let myarray = [];
 
 for(let a = 9; a>=0; a-=2){
  myarray.push(a);
 
 }
-console.log(myarray);
+console.log(myarray); */
+
+
+let number = (22);
+function even () {
+for (let a = 0; a <=number; a +=2){
+   even.push(a);
+   
+console.log(even);
+}}
