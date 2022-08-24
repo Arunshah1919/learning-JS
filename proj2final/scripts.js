5c7269b8f99ab8d580b1336cacbce8063686b91a
macbook pro <macbookpro@192.168.1.100>
2022-08-24 20:35:45 +0530

    commit

--- proj2final/scripts.js
+++ proj2final/scripts.js
@@ -3,29 +3,20 @@ const cyan = document.querySelector(".cyan");
 const violet = document.querySelector(".violet");
 const orange = document.querySelector(".orange");
 const pink = document.querySelector(".pink");
-
 const center = document.querySelector(".center");
 
-// console.log(window.getComputedStyle(red).backgroundColor);
-
-const getBGColor = (selectedElement) => {
+const getBGColor = function(selectedElement){
   return window.getComputedStyle(selectedElement).backgroundColor;
-};
+}
 
-// var color = getBGColor(pink);
+const magicColorChanger = function (element, color) {
+return element.addEventListener("mouseenter", function() {
+  center.style.background = color;
+})
+}
 
-// pink.addEventListener("mouseenter", () => {
-//   center.style.background = color;
-// });
-
-const magicColorChanger = (element, color) => {
-  return element.addEventListener("mouseenter", () => {
-    center.style.background = color;
-  });
-};
-
-magicColorChanger(red, getBGColor(red));
-magicColorChanger(cyan, getBGColor(cyan));
-magicColorChanger(violet, getBGColor(violet));
-magicColorChanger(orange, getBGColor(orange));
-magicColorChanger(pink, getBGColor(pink));
+magicColorChanger(red , getBGColor(red));
+magicColorChanger(cyan , getBGColor(cyan));
+magicColorChanger(violet , getBGColor(violet));
+magicColorChanger(orange , getBGColor(orange));
+magicColorChanger(pink , getBGColor(pink));
