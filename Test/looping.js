f6d86d0a14d7e874a7120bc92c9c019823727a77
macbook pro <macbookpro@192.168.1.100>
2022-08-25 11:26:51 +0530

    sasf

--- Test/looping.js
+++ Test/looping.js
@@ -5,21 +5,25 @@ let names = ["Arun", "Jatin", "Gaurav", 1234, "Rahul", "Vikas", "Arjun"];
 //     console.log(names[i]);
 // }
 
-// let i = 0;
-// while(i<names.length){
-//     console.log(names[i]);
-//     i++;
-// }
+ let i = 0;
+ while(i<names.length){
+     
+if(typeof names[i] !== "string" ){
+    i++; continue
+}  
+console.log(names[i]);
+     i++;
+}
 
 // for (const n of names){
 //     console.log(n);
 // }
 
-let symbols = {
-    ig: "Instagram",
-    fb: "Facebook",
-    yt: "Youtube",
-}
-for(const n in symbols){
-    console.log(`key is: ${n} and the value is: ${symbols[n]}`);
-}
+// let symbols = {
+//     ig: "Instagram",
+//     fb: "Facebook",
+//     yt: "Youtube",
+// }
+// for(const n in symbols){
+//     console.log(`key is: ${n} and the value is: ${symbols[n]}`);
+// }
