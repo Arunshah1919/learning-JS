d1430038c2bef2586f593ad0ea42681443113cb9
Arunshah1919 <arunshah1919@gmail.com>
2022-09-14 11:12:59 +0530

    dsafdvav

---
+++ osModule/improtExports.js
@@ -0,0 +1,3 @@
+const add = require ("./osmod");
+
+console.log(add);
--- osModule/osmod.js
+++ osModule/osmod.js
@@ -1,11 +1,22 @@
-const os = require("os");
-console.log(os.arch());
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.type());
+ const os = require("os");
 
-const freeMemory = os.freemem();
-console.log(`${freeMemory /1024/1024/1024}`);
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.type());
 
-const totalMemory = os.totalmem();
-console.log(`${totalMemory /1024/1024/1024}`);
+const freeMemory = os.freemem()
+console.log(`${freeMemory / 1024 / 1024 / 1024}`);
+
+// const totalMemory = os.totalmem()  
+//   (`${totalMemory / 1024 / 1024 / 1024}`)
+ 
+// //console.log(os.networkInterfaces());
+// console.log(os.release());
+
+// const upTime = os.uptime()
+// console.log(`${upTime/ 60/60}`);
+
+const Type = os.freemem();
+
+module.exports = Type;
